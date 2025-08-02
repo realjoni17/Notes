6901430de803aff011e86e09bc373a09f1161d33
joni <jaunivashisth@gmail.com>
2025-08-02 17:57:07 +0530

    updated dependencies

--- app/src/main/java/com/android/roomdbtest/domain/model/Notes.kt
+++ app/src/main/java/com/android/roomdbtest/domain/model/Notes.kt
@@ -3,9 +3,9 @@ package com.android.roomdbtest.domain.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "note")
 data class Note(
-    @PrimaryKey val id: Int? = null,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val title: String,
     val content: String,
     //val timestamp: Long,
